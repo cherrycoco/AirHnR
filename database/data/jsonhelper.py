8aa504f15c805f5b1d58b645dd045b51f6bb4060
Cody Schindler <admin@codyschindler.com>
2018-01-15 15:10:11 -0800

    updated db fake data

--- database/data/jsonhelper.py
+++ database/data/jsonhelper.py
@@ -19,3 +19,33 @@ with open('./sf_lat_long.txt', 'r') as f:
 
 with open ('./sf_lat_long.json', 'w') as ofile:
     json.dump(tobe, ofile)
+
+
+tobe = []
+with open('./tokyo_lat_long.txt', 'r') as f:
+    for line in f:
+        ll = [x for x in line.split(',')]
+        tobe.append({"latitude": ll[1], "longitude": ll[3]})
+
+with open ('./tokyo_lat_long.json', 'w') as ofile:
+    json.dump(tobe, ofile)
+
+
+tobe = []
+with open('./la_lat_long.txt', 'r') as f:
+    for line in f:
+        ll = [x for x in line.split(',')]
+        tobe.append({"latitude": ll[1], "longitude": ll[3]})
+
+with open ('./la_lat_long.json', 'w') as ofile:
+    json.dump(tobe, ofile)
+
+
+tobe = []
+with open('./cape_town_lat_long.txt', 'r') as f:
+    for line in f:
+        ll = [x for x in line.split(',')]
+        tobe.append({"latitude": ll[1], "longitude": ll[3]})
+
+with open ('./cape_town_lat_long.json', 'w') as ofile:
+    json.dump(tobe, ofile)
